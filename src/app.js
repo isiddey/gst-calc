da30484457b743afe4754bec91e55b4e4d5ae514
isiddey <siddhantmishra@outlook.com>
2017-10-05 19:29:36 +0530

    Added electron + react.

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (<div>
+            <h1>Hello World!</h1>
+            This is being loaded from src/app.js aka React render.
+    </div>);
+  }
+}
